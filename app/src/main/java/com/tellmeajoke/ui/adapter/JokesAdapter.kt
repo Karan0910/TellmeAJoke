646ec1a54aae2cbf75edb17f1da3246a31431319
Karan Sharma <95sharma.karan@gmail.com>
2021-10-11 23:20:39 -0400

    Add fav jokes recycler view and adapter

---
+++ app/src/main/java/com/tellmeajoke/ui/adapter/JokesAdapter.kt
@@ -0,0 +1,42 @@
+package com.tellmeajoke.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.tellmeajoke.data.db.entities.FavouriteJoke
+import com.tellmeajoke.databinding.ItemFavJokeBinding
+
+val listDiffer = object : DiffUtil.ItemCallback<FavouriteJoke>() {
+    override fun areItemsTheSame(oldItem: FavouriteJoke, newItem: FavouriteJoke): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: FavouriteJoke, newItem: FavouriteJoke): Boolean =
+        oldItem == newItem
+}
+
+class JokesAdapter : ListAdapter<FavouriteJoke, JokeViewHolder>(listDiffer) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+        val binding =
+            ItemFavJokeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return JokeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class JokeViewHolder(private val binding: ItemFavJokeBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    internal fun bind(joke: FavouriteJoke) {
+        binding.textJoke.text = joke.joke
+
+        itemView.setOnClickListener {
+
+        }
+    }
+}
