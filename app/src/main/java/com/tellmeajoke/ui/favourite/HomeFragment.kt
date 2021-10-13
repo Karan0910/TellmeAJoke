16b40c6124a7fdeaf7582a1a3d230a42eff5dd75
Karan Sharma <95sharma.karan@gmail.com>
2021-10-12 21:23:27 -0400

    Fix minor UI issue and code refactor

--- app/src/main/java/com/tellmeajoke/ui/favourite/HomeFragment.kt
+++ app/src/main/java/com/tellmeajoke/ui/favourite/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.tellmeajoke.ui.favourite
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tellmeajoke.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -23,6 +24,9 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var textView: TextView
+    private lateinit var favButton: ImageView
+    private lateinit var refreshButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,11 +39,39 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textJoke
-        val favButton : ImageView = binding.favButton
+        initViews()
+        observerData()
+        homeViewModel.fetchJoke() // Fetch random joke
 
+        onClickListener()
+
+        return root
+    }
+
+    private fun onClickListener() {
+
+        refreshButton.setOnClickListener {
+            homeViewModel.fetchJoke()
+        }
+        favButton.setOnClickListener {
+            homeViewModel.addJoke()
+        }
+
+        binding.shareJokeBt.setOnClickListener {
+
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, homeViewModel.joke.value?.joke)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+    }
+
+    private fun observerData() {
         homeViewModel.joke.observe(viewLifecycleOwner, Observer {
-            println(it)
             textView.text = it.joke
             favButton.isSelected = it.isFav
         })
@@ -51,19 +83,12 @@ class HomeFragment : Fragment() {
                 binding.progressBar.visibility = View.INVISIBLE
             }
         }
+    }
 
-        val refreshButton : Button = binding.refreshButton
-
-        refreshButton.setOnClickListener {
-            homeViewModel.fetchJoke()
-        }
-        favButton.setOnClickListener {
-            homeViewModel.addJoke()
-        }
-
-        homeViewModel.fetchJoke()
-
-        return root
+    private fun initViews() {
+        textView = binding.textJoke
+        favButton = binding.favButton
+        refreshButton = binding.refreshButton
     }
 
     override fun onDestroyView() {
